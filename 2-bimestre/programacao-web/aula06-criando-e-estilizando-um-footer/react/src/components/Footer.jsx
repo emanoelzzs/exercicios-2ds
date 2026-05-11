5b927fe479267db2734d47b3bc3660c45767939e
Emanoel Alves <emanoelkenon@gmail.com>
2026-05-10 21:39:52 -0300

    atividade feita

---
+++ 2-bimestre/programacao-web/aula06-criando-e-estilizando-um-footer/react/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "../styles/footer.css"
+function Footer() {
+    return(
+        <>
+        <footer>
+            <p>Todos os direitos reservados - 2026</p>
+        </footer>
+        </>
+    )
+}
+
+export default Footer
