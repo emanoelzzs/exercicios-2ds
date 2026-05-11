c1abaa9d2980a48dcfa6215406a3294cbd1e95dd
Emanoel Alves <joao.silva3984@aluno.ce.gov.br>
2026-05-11 19:56:42 +0000

    feito

--- 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
@@ -4,13 +4,31 @@ import Tim from "./../public/TIMMM2.jpg"
 export default function App() {
     return(
         <>
+        <div className="profile-card">
         < Profile
-        nome="Manel"
+        nome="João Berck"
         idade={18}
         profissao="Atendente da TIM"
         imagem={Tim}
         descricao="Menino loiro"
         />
+
+        < Profile
+        nome="Ana Maria"
+        idade={35}
+        profissao="Professora de geografia"
+        imagem={Tim}
+        descricao="mulher esbelta"
+        />
+
+        < Profile
+        nome="Valdonis"
+        idade={40}
+        profissao="Policial"
+        imagem={Tim}
+        descricao="Homem forte"
+       /> 
+       </div>
 
         </>
     )
--- 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/components/Profile.jsx
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import "../styles/profile.css"
 function Profile({nome, idade, profissao, imagem, descricao}) {
         return(
             <>
-            <div className="profile-card">
+            <div>
                 <div className="card">
                     <img src={imagem} alt={descricao} />
                     <p className="nome">{nome}</p>
