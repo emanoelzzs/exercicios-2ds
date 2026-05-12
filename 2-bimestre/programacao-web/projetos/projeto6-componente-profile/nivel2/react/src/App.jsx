5fcd384253177d98f42a38e39fb612556b286062
Emanoel Alves <emanoelkenon@gmail.com>
2026-05-11 23:10:52 -0300

    atualizando projeto 6

--- 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/App.jsx
@@ -1,23 +1,25 @@
 import Profile from "./components/Profile"
-import Tim from "./../public/TIMMM2.jpg"
+import Titulo from "./components/Titulo"
 
 export default function App() {
     return(
         <>
+        < Titulo/>
+        
         <div className="profile-card">
         < Profile
-        nome="João Berck"
+        nome="Ana Heloisa"
         idade={18}
-        profissao="Atendente da TIM"
-        imagem={Tim}
-        descricao="Menino loiro"
+        profissao="Influencer"
+        imagem="https://diariodecuiaba.nyc3.digitaloceanspaces.com/storage/webdisco/2025/10/08/800x600/f54ca1ccb0d5acf19f3a6d3c5763e5cd-diariodecuiaba-com-br.jpg"
+        descricao="influenciadora"
         />
 
         < Profile
         nome="Ana Maria"
         idade={35}
         profissao="Professora de geografia"
-        imagem={Tim}
+        imagem="https://thumbs.dreamstime.com/b/mujer-rubia-natural-22149492.jpg"
         descricao="mulher esbelta"
         />
 
@@ -25,7 +27,7 @@ export default function App() {
         nome="Valdonis"
         idade={40}
         profissao="Policial"
-        imagem={Tim}
+        imagem="https://estiloeatitude.com.br/wp-content/uploads/2024/07/como-ser-um-homem-elegante-capa.webp"
         descricao="Homem forte"
        /> 
        </div>
---
+++ 2-bimestre/programacao-web/projetos/projeto6-componente-profile/nivel2/react/src/components/Titulo.jsx
@@ -0,0 +1,12 @@
+import "../styles/titulo.css"
+
+function Titulo() {
+    return(
+        <>
+            <h1>Usuários do sistema</h1>
+            <h2>Conheça nossa equipe</h2>
+        </>
+    )
+}
+
+export default Titulo
