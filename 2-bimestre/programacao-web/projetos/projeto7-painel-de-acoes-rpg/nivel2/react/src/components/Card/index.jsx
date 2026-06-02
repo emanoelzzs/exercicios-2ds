cb11e808594c702de96bcbaa399aa2f6cd7cc8be
Emanoel Alves <joao.silva3984@aluno.ce.gov.br>
2026-06-02 13:36:00 +0000

    fazendo

--- 2-bimestre/programacao-web/projetos/projeto7-painel-de-acoes-rpg/nivel2/react/src/components/Card/index.jsx
+++ 2-bimestre/programacao-web/projetos/projeto7-painel-de-acoes-rpg/nivel2/react/src/components/Card/index.jsx
@@ -4,18 +4,26 @@ import "./style.css"
 export default function Card({ children, classe, nivel, vida, experiencia, img }){
     return(
         <>
+        <div className="card">
+
         <div className="card-perfil">
+
+            <div className="perfil">
             <img src={img}/>
-            <div className="infos">
-            <h1>{children}</h1>
-            <p>Classe: {classe}</p>
-            <p>Nível: {nivel} </p>
-            <p>Vida: {vida}</p>
-            <p>Experiência: {experiencia}</p>
-            <div className="botoes">
-             <Botao className="bnt"/>
-             </div>
+                <div className="infos">
+                    <h1>{children}</h1>
+                    <p>Classe: {classe}</p>
+                    <p>Nível: {nivel} </p>
+                    <p>Vida: {vida}</p>
+                    <p>Experiência: {experiencia}</p>
+                </div>
             </div>
+            <div className="botoes">
+                <Botao className="bnt"/>
+             </div>
+
+            </div> 
+        
            
         </div>
         </>
