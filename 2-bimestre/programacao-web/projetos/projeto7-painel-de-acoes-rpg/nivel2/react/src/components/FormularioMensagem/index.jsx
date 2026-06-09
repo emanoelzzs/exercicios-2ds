dabeef70613a6c9776d83a6508e43c5bfbd425c8
Emanoel Alves <emanoelkenon@gmail.com>
2026-06-08 21:16:27 -0300

    feito

--- 2-bimestre/programacao-web/projetos/projeto7-painel-de-acoes-rpg/nivel2/react/src/components/FormularioMensagem/index.jsx
+++ 2-bimestre/programacao-web/projetos/projeto7-painel-de-acoes-rpg/nivel2/react/src/components/FormularioMensagem/index.jsx
@@ -1,52 +1,47 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-export default function FormularioMensagem(){ 
-=======
-=======
 import "./style.css"
->>>>>>> e740abb5b08f0b13dfd868f86f3ea8c2bc0e46cd
-export default function FormularioMensagem(){
-        
-        
->>>>>>> 33b3df2ef67cc8a5397b31bf74f7519f91d674e4
-    function enviar(event){
-        const nome = document.getElementById("nome").value
-        const mensagem = document.getElementById("mensagem").value
-        event.preventDefault()
-        const nome = document.getElementById("nome").value
-        const mensagem = document.getElementById("mensagem").value
-        alert(`${nome} sua mensagem:"${mensagem}" foi enviada`)
-    }
-    return(
-        <div className="bloco">
-        <div className="formulario">
-        <header>
-        <h1>Formulário de Mensagem</h1>
-        </header>
-<<<<<<< HEAD
-        <form onSubmit={enviar}>
-            <input type="text" placeholder="Digite seu nome" id="nome"/>
-            <input type="text" placeholder="Digite sua mensagem..." id="mensagem"/>
-            <button type="submit">Envia mensagem</button>
-        </form>
-        </>
-=======
-                <form onSubmit={enviar}>
 
+export default function FormularioMensagem() {
+
+    function enviar(event) {
+        event.preventDefault()
+
+        const nome = document.getElementById("nome").value
+        const mensagem = document.getElementById("mensagem").value
+
+        alert(`${nome} sua mensagem: "${mensagem}" foi enviada`)
+    }
+
+    return (
+        <div className="bloco">
+            <div className="formulario">
+                <header>
+                    <h1>Formulário de Mensagem</h1>
+                </header>
+
+                <form onSubmit={enviar}>
                     <div className="input">
                         <label>NOME:</label>
-                        <input type="text" placeholder="Digite seu nome" id="nome"/>
+                        <input
+                            type="text"
+                            placeholder="Digite seu nome"
+                            id="nome"
+                        />
                     </div>
 
                     <div className="input">
                         <label>MENSAGEM:</label>
-                        <input type="text" placeholder="Digite sua mensagem..." id="mensagem"/>
+                        <input
+                            type="text"
+                            placeholder="Digite sua mensagem..."
+                            id="mensagem"
+                        />
                     </div>
 
-                    <button type="submit" className="enviar">enviar mensagem</button>
+                    <button type="submit" className="enviar">
+                        Enviar mensagem
+                    </button>
                 </form>
+            </div>
         </div>
-        </div>
->>>>>>> e740abb5b08f0b13dfd868f86f3ea8c2bc0e46cd
     )
 }
